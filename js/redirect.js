70784d2b9bd0e3edac21af7d9a0fe305eebb1c45
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-01-23 15:42:16 -0300

    Reatualizando caminhos

--- js/redirect.js
+++ js/redirect.js
@@ -1,5 +1,5 @@
 // se o html estiver dentro da pasta "html" então vai ser com ponto simples senão é com dois pontos
-let caminhoHtml = ['/html/register.html', '/html/login.html','../html/register.html', '../html/login.html'];
+let caminhoHtml = ['html/register.html', 'html/login.html','../html/register.html', '../html/login.html'];
 function redirecionar(n){
     window.location.href = caminhoHtml[n];
 }
